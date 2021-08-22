fcf63f06ccd7267d31d63ba98ff812d53a63dc5a
KalynAnne <kalyn.barry@my.ju.edu.au>
2021-08-22 20:20:52 +1000

    Refactor password check program to use functions

--- Prac_03/password_checker.py
+++ Prac_03/password_checker.py
@@ -1,10 +1,23 @@
 """Password checker"""
 
 MIN_LENGTH = 6
-password = input("Input Password: ")
-while len(password) < MIN_LENGTH:
-    print("INVALID LENGTH")
-    password = input("Input Password: ")
-for i in range(len(password)):
-    print("*", end='')
 
+
+def main():
+    password = get_password()
+    while len(password) < MIN_LENGTH:
+        print("INVALID LENGTH")
+        password = get_password()
+    get_asterisks(password)
+
+
+def get_password():
+    return input("Input Password: ")
+
+
+def get_asterisks(password):
+    for i in range(len(password)):
+        print("*", end='')
+
+
+main()
