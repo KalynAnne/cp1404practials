0ddb8a33ee17440170b61aee5085ba0398ef5033
KalynAnne <kalyn.barry@my.ju.edu.au>
2021-09-24 10:28:28 +1000

    Add KivyDemos-master files

---
+++ tester.py
@@ -0,0 +1,9 @@
+# names = ["Benn", "Dillon", "Jay", "Kalyn"]
+# ages = [23, 23, 20, 20]
+# largest_age = max(ages)
+# print(largest_age)
+# index = ages.index(largest_age)
+# print(index)
+# print("The eldest/s are", names[index])
+
+
